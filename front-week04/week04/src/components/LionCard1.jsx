53c0581929dae5f71e1391937a0003d9d6432645
ynzung <syj91105@sookmyung.ac.kr>
2025-05-22 23:28:34 +0900

    Feat: 프론트 4주차 세나 LionCard1 컴포넌트 생성

--- front-week04/week04/src/components/LionCard1.jsx
+++ front-week04/week04/src/components/LionCard1.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import lion1 from "../assets/lion1.jpg";
+import * as S from "./styled";
 
 const LionCard1 = () => {
   const isReal = true;
   return (
-    <div>
-      <img src={lion1} alt="사자1" />
+    <S.Card>
+      <S.Img src={lion1} alt="사자1" />
       <h1>1번 사자</h1>
       <p>{isReal ? "진짜 아기사자" : "가짜 아기사자"}</p>
-    </div>
+    </S.Card>
   );
 };
 
