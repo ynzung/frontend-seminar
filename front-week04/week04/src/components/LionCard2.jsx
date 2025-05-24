80dbae78cde069a052d443cb572972b8c25d0693
ynzung <syj91105@sookmyung.ac.kr>
2025-05-24 17:37:28 +0900

    Feat: 프론트 4주차 세미나 LionCard2 컴포넌트 생성

--- front-week04/week04/src/components/LionCard2.jsx
+++ front-week04/week04/src/components/LionCard2.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import lion2 from "../assets/lion2.jpg";
+import * as S from "./styled";
 
 const LionCard2 = () => {
   const isReal = true;
   return (
-    <div>
-      <img src={lion2} alt="사자2" />
+    <S.Card>
+      <S.Img src={lion2} alt="사자2" />
       <h1>2번 사자</h1>
       <p>{isReal ? "진짜 아기사자" : "가짜 아기사자"}</p>
-    </div>
+    </S.Card>
   );
 };
 
