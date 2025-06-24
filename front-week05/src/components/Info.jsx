1c75edd720c556f6a6946afb98e9e2a5902c1b9d
ynzung <syj91105@sookmyung.ac.kr>
2025-06-24 16:12:49 +0900

    [Feat] useEffect 관련 기능 구현

--- front-week05/src/components/Info.jsx
+++ front-week05/src/components/Info.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+
+  //   useEffect(() => {
+  //     console.log("렌더링이 완료되었습니다.");
+  //     console.log({ name, nickname });
+  //   });
+
+  //   useEffect(() => {
+  //     console.log("마운트 될 때만 실행됩니다.");
+  //   }, []);
+
+  useEffect(() => {
+    console.log(nickname);
+  }, [nickname]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
