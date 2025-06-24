5dde7327a22b1dea00b9b71e1f0b8699233f52cd
ynzung <syj91105@sookmyung.ac.kr>
2025-06-24 15:16:47 +0900

    [Feat] props 실습 완료

--- front-week05/src/App.jsx
+++ front-week05/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Hello from "./components/Hello";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Hello name={"숙멋사"} />
+    </>
+  );
 }
 
 export default App;
